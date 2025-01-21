9b01386bc40dce3b6ec9c7ae8403ce92c3eab01f
Gabriel Arias <gaburieru14xdd@gmail.com>
2025-01-21 07:31:47 -0500

    Actualiza la configuración del servidor para usar la aplicación de eventos y mejora la definición del Procfile

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'events',
+    'backend.events.apps.EventsConfig',
 ]
 
 MIDDLEWARE = [
